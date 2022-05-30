b8852637b5795b95d4c73b3f8659af012bda2fb7
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-05-30 23:20:30 +0300

    adds promisification

--- src/js/promise-api.js
+++ src/js/promise-api.js
@@ -1,16 +1,16 @@
 import '../css/work-common.css';
 
-const promise = new Promise((resolve, reject) => {
-  const canFulfilles = Math.random() > 0.5;
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfilles = Math.random() > 0.5;
 
-  setTimeout(() => {
-    if (canFulfilles) {
-      resolve('Промис выполнился успешно (fulfilled)');
-    }
+//   setTimeout(() => {
+//     if (canFulfilles) {
+//       resolve('Промис выполнился успешно (fulfilled)');
+//     }
 
-    reject('Промис выполнился с ошибкой (rejected)');
-  }, 2000);
-});
+//     reject('Промис выполнился с ошибкой (rejected)');
+//   }, 2000);
+// });
 
 // then(onSuccess, onError);
 // console.log(`✅ ${result}`);
@@ -18,13 +18,13 @@ const promise = new Promise((resolve, reject) => {
 
 // promise.then(onFulfilled, onRejected);
 
-function onFulfilled(result) {
-  console.log(result);
-}
+// function onFulfilled(result) {
+//   console.log(result);
+// }
 
-function onRejected(error) {
-  console.log(error);
-}
+// function onRejected(error) {
+//   console.log(error);
+// }
 
 // =================Цепочки промисов (chaining)=============
 // =================Promise.prototype.catch(error)==========
@@ -65,15 +65,15 @@ function onRejected(error) {
 
 // ===============================================
 
-promise
-  .then(onFulfilled)
-  .then(x => {
-    console.log(x);
-    // throw new Error('ошибка во втором then');
-    return 20;
-  })
-  .then(y => console.log(y))
-  .catch(error => console.log(error))
-  .finally(() => console.log('Я буду выполнен в любом случае'));
+// promise
+//   .then(onFulfilled)
+//   .then(x => {
+//     console.log(x);
+//     // throw new Error('ошибка во втором then');
+//     return 20;
+//   })
+//   .then(y => console.log(y))
+//   .catch(error => console.log(error))
+//   .finally(() => console.log('Я буду выполнен в любом случае'));
 
 // ! В then выносим только успешное выполнение промиса, а неуспешное в catch, в конце цепочки.
