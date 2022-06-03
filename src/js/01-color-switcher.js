a89d9e1872f8908a303bd850af801ff9c4fb2fa0
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-06-04 00:57:30 +0300

    adds solution for "color-switcher"

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
-import '../css/common.css'
+import '../css/common.css';
+
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на случайное значение используя инлайн стиль. При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
+
+// ⚠️ Учти, на кнопку «Start» можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled).
+
+const TIME_DELAY = 1000;
+let intervalID = null;
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onChangeColor);
+refs.stopBtn.addEventListener('click', onStopChange);
+
+function onChangeColor() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, TIME_DELAY);
+}
+
+function onStopChange(e) {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(intervalID);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import '../css/common.css';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
